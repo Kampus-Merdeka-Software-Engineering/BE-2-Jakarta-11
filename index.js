b00843e5220a389f713307cf015727f969f8cb57
HilmanZy <hilmanfatoni8@gmail.com>
2023-11-26 06:23:04 +0700

    new_server

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
+// Middleware to handle CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://magenta-rose-raven-hat.cyclic.app/'); // Sesuaikan dengan origin yang benar
+  res.header('Access-Control-Allow-Origin', '*'); // Adjust as needed
   res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
@@ -13,22 +14,36 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+// Storing user data in an array and maintaining a counter for ID
 const users = [];
+let userIdCounter = 1;
 
+// Endpoint for handling form submission
 app.post('/submit', (req, res) => {
   try {
     const userData = req.body;
-    users.push(userData);
-    res.json({ message: 'Data berhasil disimpan', data: userData });
+    const id = userIdCounter++; // Assign an ID but don't send it back
+
+    // Store with ID but send back data without ID
+    users.push({ ID: id, ...userData });
+
+    res.json({ message: 'Data successfully stored', data: userData });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
+// Endpoint to retrieve all user data in JSON array format
 app.get('/users', (req, res) => {
-  res.json({ users });
+  res.json(users);
+});
+
+// Main URL displaying all data in formatted JSON
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(users, null, 2)); // Formatting JSON output
 });
 
 app.listen(port, () => {
-  console.log(`Server berjalan pada port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
