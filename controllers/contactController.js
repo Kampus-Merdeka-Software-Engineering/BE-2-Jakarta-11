db1967bb0866e838142d2c2c89a66aa339f6c878
HilmanZy <hilmanfatoni8@gmail.com>
2023-12-03 12:19:54 +0700

    newest03

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,11 @@
-// In controllers/contactController.js
-
 exports.createContact = async (req, res) => {
   try {
+    console.log("Received data:", req.body); // Log untuk debugging
     const data = req.body;
     const savedContact = await contactService.addContact(data);
     res.status(200).json({ message: 'Contact saved successfully', contact: savedContact });
   } catch (error) {
+    console.error("Error in createContact:", error); // Log error
     res.status(500).send(error.message);
   }
 };
